a1eb0e85f5640036cb7ecc029f8827d2784807ef
alex-njuguna <njugunakinuthia013@gmail.com>
2023-10-07 21:52:20 +0300

    create and save a new post

--- blog/app/controllers/blog_posts_controller.rb
+++ blog/app/controllers/blog_posts_controller.rb
@@ -8,4 +8,23 @@ class BlogPostsController < ApplicationController
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
+
+    def new
+        @blog_post = BlogPost.new
+    end
+
+    def create
+        @blog_post = BlogPost.new(blog_post_params)
+        if @blog_post.save
+            redirect_to @blog_post
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def blog_post_params
+        params.require(:blog_post).permit(:title, :body)
+    end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   # Defines the root path route ("/")
   root "blog_posts#index"
