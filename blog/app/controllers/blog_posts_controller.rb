b080d19f551ae0bdfa9d4f3f4b3ffd9cf588c89d
alex-njuguna <njugunakinuthia013@gmail.com>
2023-10-07 15:53:20 +0300

    link to a blog

--- blog/app/controllers/blog_posts_controller.rb
+++ blog/app/controllers/blog_posts_controller.rb
@@ -2,4 +2,10 @@ class BlogPostsController < ApplicationController
     def index
         @blog_posts = BlogPost.all
     end
+
+    def show
+        @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
