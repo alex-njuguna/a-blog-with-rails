4202f2785c89a8a6db1f96e089c076805b456b3b
alex-njuguna <njugunakinuthia013@gmail.com>
2023-10-16 14:13:53 +0300

    comments deletion and authentication

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,9 +1,20 @@
 class CommentsController < ApplicationController
+
+	http_basic_authenticate_with name: "admin", password: "admin", only: :destroy 
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
 
 		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+
+		redirect_to article_path(@article), status: :see_other
 	end
 
 	private
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 
 	include Visible
 
-	has_many :comments
+	has_many :comments, dependent: :destroy
 
 	validates :title, presence: true, length: {minimum: 10}
 	validates :body, presence:true, length: {minimum: 20}
